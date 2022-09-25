af3b1126eeaa446e7a852f1bb45342d6974ad645
negiashish1 <negi.ashish012@gmail.com>
2022-09-26 02:08:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from './components/header/Header'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
 	  <About />
 	  <Experience />
 	  <Services />
+	  <Contact />
+      <Footer />
     </>
   )
 }
