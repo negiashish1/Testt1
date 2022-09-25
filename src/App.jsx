b90229fad652fa98d0ad423be1615d259f2dff74
negiashish1 <negi.ashish012@gmail.com>
2022-09-26 02:05:55 +0530

    test t4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from './components/header/Header'
 import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
 
 const App = () => {
   return (
     <>
       <Header />
 	  <About />
+	  <Experience />
+	  <Services />
     </>
   )
 }
