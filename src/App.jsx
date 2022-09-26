0ca020dee40935b83763454c4ba9f11ee401df4a
negiashish1 <negi.ashish012@gmail.com>
2022-09-27 01:37:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
 	  <Services />
 	  <Portfolio />
 	  <Testimonials />
+	  <Contact />
       <Footer />
     </>
   )
