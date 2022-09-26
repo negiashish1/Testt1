41bdc7f68e6763e378613f8cb2ca3285165a2bf0
negiashish1 <negi.ashish012@gmail.com>
2022-09-27 02:49:07 +0530

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './contact.css'
 import {AiOutlineMail} from 'react-icons/ai'
+import {GrChat} from 'react-icons/gr'
 import {BsWhatsapp} from 'react-icons/bs'
 import emailjs from 'emailjs-com';
 import {useRef} from 'react';
@@ -36,7 +37,7 @@ const Contact = () => {
           </article>
 
           <article className='contact__option'>
-            <BsWhatsapp className='contact__option-icon'/>
+            <GrChat className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>shivanimadwal</h5>
             <a href="https://www.facebook.com/ashish.negi.12914216" target="__blank">Send a message</a>
