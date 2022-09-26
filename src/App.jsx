8fdbaaab6acf752a296d3514e907bde361156968
negiashish1 <negi.ashish012@gmail.com>
2022-09-27 01:31:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
@@ -12,7 +11,6 @@ const App = () => {
   return (
     <>
       <Header />
-	  <Nav />
 	  <About />
 	  <Experience />
 	  <Services />
