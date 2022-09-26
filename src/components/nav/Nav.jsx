a372338d0548845dd82e373d67f5a065c1f12854
negiashish1 <negi.ashish012@gmail.com>
2022-09-27 02:32:46 +0530

    Update Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,7 +4,6 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {BiMessageSquareDetail} from 'react-icons/bi'
-import {BiMessageSquareDetail} from 'react-icons/tb'
 
 import {useState} from 'react'
 
