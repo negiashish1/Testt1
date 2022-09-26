12faf3aa80b17d6902b205bbe65679a56fe04d3b
negiashish1 <negi.ashish012@gmail.com>
2022-09-27 01:00:04 +0530

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './contact.css'
-import {MdOutlineEmail} from 'react-icons/md'
-import {RiMessengerLine} from 'react-icons/ri'
-import {BsWhatsapp} from 'react-icons/bs'
+// import {MdOutlineEmail} from 'react-icons/md'
+// import {RiMessengerLine} from 'react-icons/ri'
+// import {BsWhatsapp} from 'react-icons/bs'
 // import emailjs from 'emailjs-com';
 // import {useRef} from 'react';
 
@@ -30,21 +30,21 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className='contact__option'>
-            <MdOutlineEmail className='contact__option-icon'/>
+            {/* <MdOutlineEmail className='contact__option-icon'/> */}
             <h4>Email</h4>
             <h5>goofi.frank1@gmail.com</h5>
             <a href="mailto: goofi.frank1@gmail.com" target="__blank">Send a message</a>
           </article>
 
           <article className='contact__option'>
-            <RiMessengerLine className='contact__option-icon'/>
+            {/* <RiMessengerLine className='contact__option-icon'/> */}
             <h4>Messenger</h4>
             <h5>shivanimadwal</h5>
             <a href="https://www.facebook.com/ashish.negi.12914216" target="__blank">Send a message</a>
           </article>
 
           <article className='contact__option'>
-            <BsWhatsapp className='contact__option-icon'/>
+            {/* <BsWhatsapp className='contact__option-icon'/> */}
             <h4>WhatsApp</h4>
             <h5>+91 **********</h5>
             <a href="https://api.whatsapp.com/send?phone=+919643935426" target ="__blank">Send a message</a>
